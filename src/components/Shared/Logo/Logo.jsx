78b0eb8d9438088f7e175daf66024ebf2315842c
Rakibul29h <rakibul2170@gmail.com>
2025-12-06 20:18:33 +0600

    navbar is added with logo

---
+++ src/components/Shared/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import { ChefHat } from 'lucide-react';
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className='flex items-center gap-2'>
+             <ChefHat color='#ff6900' strokeWidth={3} size={40} />
+             <h3 className='text-2xl font-bold'>LocalChef<span className='text-primary'>Bazaar</span></h3>
+        </div>
+    );
+};
+
+export default Logo;
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Shared/Navbar/Navbar"
+
+
+
+
+const MainLayout = () => {
+  return (
+    <div className="">
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default MainLayout
