518f077587a47cb453df55ad028bb29236322ca9
Rakibul29h <rakibul2170@gmail.com>
2025-12-06 20:54:08 +0600

    login page and sign up page design done

---
+++ src/Pages/LogIn/LogIn.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router";
+import Button from "../../components/Shared/Button/Button";
+
+const LogIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data)
+  };
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-white">
+      <div className="flex flex-col max-w-md md:min-w-[400px] p-6 rounded-md sm:p-10 bg-gray-50 text-gray-900">
+        <div className="mb-8 text-center">
+          <h1 className="my-3 text-4xl font-bold">Log In</h1>
+          <p className="text-sm text-gray-500">Welcome to ChefLocalBazaar</p>
+        </div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate=""
+          action=""
+          className="space-y-6 ng-untouched ng-pristine ng-valid"
+        >
+          <div className="space-y-4">
+           
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm">
+                Email address
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter Your Email Here"
+                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-orange-200 bg-gray-100 text-gray-900"
+                data-temp-mail-org="0"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: "Please enter a valid email address.",
+                  },
+                })}
+              />
+              {errors.email && (
+                <p className="text-red-500 text-xs mt-1">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+     
+            <div>
+              <div className="flex justify-between">
+                <label htmlFor="password" className="text-sm mb-2">
+                  Password
+                </label>
+              </div>
+              <input
+                type="password"
+                autoComplete="new-password"
+                id="password"
+                placeholder="*******"
+                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-orange-200 bg-gray-100 text-gray-900"
+                {...register("password", {
+                  required: "Password is required",
+                 
+                })}
+              />
+              {errors.password && (
+                <p className="text-red-500 text-xs mt-1">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+          </div>
+
+          <div>
+            {/* <button
+              type='submit'
+              className='bg-lime-500 w-full rounded-md py-3 text-white'
+            >
+              {loading ? (
+                <TbFidgetSpinner className='animate-spin m-auto' />
+              ) : (
+                'Continue'
+              )}
+            </button> */}
+            <Button label="Continue"></Button>
+          </div>
+        </form>
+
+        <p className="px-6 text-sm text-center text-gray-400">
+          Already have an account?{" "}
+          <Link
+            to="/login"
+            className="hover:underline hover:text-lime-500 text-gray-600"
+          >
+            Login
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LogIn;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,8 @@
 
 import { createBrowserRouter } from 'react-router'
 import MainLayout from '../layouts/MainLayout'
+import LogIn from '../Pages/LogIn/LogIn'
+import SignUp from '../Pages/SignUp/SignUp'
 
 
 export const router = createBrowserRouter([
@@ -8,6 +10,8 @@ export const router = createBrowserRouter([
     path: '/',
     element: <MainLayout />,
     
-  }
+  },
+   { path: '/login', element: <LogIn /> },
+  { path: '/signup', element: <SignUp /> },
  
 ])
