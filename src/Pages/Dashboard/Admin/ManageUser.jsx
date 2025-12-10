2d1714af74a82c1f3028ad0f758e2d9f307ab76c
Rakibul29h <rakibul2170@gmail.com>
2025-12-10 20:00:35 +0600

    Make the User as a Fraud

--- src/Pages/Dashboard/Admin/ManageUser.jsx
+++ src/Pages/Dashboard/Admin/ManageUser.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import ManageUserTable from '../../../components/DashBoard/Table/ManageUserTable';
 
 const ManageUser = () => {
     return (
         <div>
             <h2 className='text-2xl font-semibold md:text-3xl '>Manage Users</h2>
-            
+            <div className='mt-10'>
+
+            <ManageUserTable></ManageUserTable>
+            </div>
         </div>
     );
 };
--- src/components/DashBoard/Table/ManageUserTable.jsx
+++ src/components/DashBoard/Table/ManageUserTable.jsx
@@ -1,46 +1,67 @@
-import React from 'react';
+import React from "react";
+import { ShoppingBag, UserLock } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useSecureAxios";
+import toast from "react-hot-toast";
 
 const ManageUserTable = () => {
-    return (
-        <div>
-            <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-      </tr>
-    </thead>
-    <tbody>
-      {/* row 1 */}
-      <tr className="bg-base-200">
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      {/* row 2 */}
-      <tr>
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      {/* row 3 */}
-      <tr>
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-        </div>
-    );
+
+    const {user}=useAuth();
+    const axiosSecure=useAxiosSecure()
+    const {data:usersList}=useQuery({
+        queryKey:["usersList",user],
+        queryFn: async()=>{
+            const result =await axiosSecure("/users")
+            return result.data;
+        }
+    })
+
+    const makeFraud=(id)=>{
+        axiosSecure.patch(`/user/makeFraud?id=${id}`)
+        .then(res=>{
+            if(res.data.modifiedCount)
+            {
+                toast.success("User marked as Fraud successfully.")
+            }
+        })
+
+    }
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr className="bg-base-300">
+              <th>User</th>
+              <th>Role</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+
+            {
+                usersList.map(user=><tr key={user._id} className="bg-gray-50 hover:bg-orange-50">
+              <td>
+                <h2 className="text-lg font-semibold ">{user.name}</h2>
+                <p className="text-gray-500">{user.email}</p>
+              </td>
+              <td>{user.role}</td>
+              <td ><span className={`${user.status==="Active"?"bg-green-200  text-green-900":" bg-red-400 "} px-3 py-2 rounded-full font-semibold `}>{user.status}</span></td>
+              <td>{user.role === "Admin" || <button disabled={user.status ==="Fraud"} onClick={()=>{
+                makeFraud(user._id)
+              }} className={`bg-red-200 hover:bg-red-300 cursor-pointer px-3 py-2 rounded-full text-red-800 font-semibold disabled:bg-gray-200 disabled:text-gray-400 disabled:cursor-not-allowed`}> Make fraud</button>}</td>
+            </tr>)
+            }
+            
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageUserTable;
